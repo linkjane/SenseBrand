2b7d8b1e873e032601f0e56b2323a71e5ee57676
link <916969890@qq.com>
2018-04-30 09:15:56 +0800

    去掉modal黑暗主题, 添加coreui到项目

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -3,7 +3,7 @@ import './vendor.ts';
 import { NgModule, Injector } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { Ng2Webstorage, LocalStorageService, SessionStorageService  } from 'ngx-webstorage';
+import { Ng2Webstorage, LocalStorageService, SessionStorageService } from 'ngx-webstorage';
 import { JhiEventManager } from 'ng-jhipster';
 
 import { NgxSpinnerModule, NgxSpinnerService } from 'ngx-spinner';
@@ -14,12 +14,21 @@ import { ErrorHandlerInterceptor } from './blocks/interceptor/errorhandler.inter
 import { NotificationInterceptor } from './blocks/interceptor/notification.interceptor';
 import { LoadingInterceptor } from './blocks/interceptor/loading.interceptor';
 import { SenseBrandSharedModule, UserRouteAccessService } from './shared';
-import { SenseBrandAppRoutingModule} from './app-routing.module';
+import { SenseBrandAppRoutingModule } from './app-routing.module';
 import { SenseBrandHomeModule } from './home/home.module';
 import { SenseBrandAdminModule } from './admin/admin.module';
 import { SenseBrandAccountModule } from './account/account.module';
 import { SenseBrandEntityModule } from './entities/entity.module';
 import { PaginationConfig } from './blocks/config/uib-pagination.config';
+
+import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
+import { PERFECT_SCROLLBAR_CONFIG } from 'ngx-perfect-scrollbar';
+import { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
+
+const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
+    suppressScrollX: true
+};
+
 // jhipster-needle-angular-add-module-import JHipster will add new module here
 import {
     JhiMainComponent,
@@ -30,17 +39,23 @@ import {
     ActiveMenuDirective,
     ErrorComponent
 } from './layouts';
+import { AppSidebarModule } from '@coreui/angular';
+import { ModalModule, TabsModule } from 'ngx-bootstrap';
 
 @NgModule({
     imports: [
         BrowserModule,
         SenseBrandAppRoutingModule,
-        Ng2Webstorage.forRoot({ prefix: 'jhi', separator: '-'}),
+        Ng2Webstorage.forRoot({prefix: 'jhi', separator: '-'}),
         SenseBrandSharedModule,
         SenseBrandHomeModule,
         SenseBrandAdminModule,
         SenseBrandAccountModule,
         SenseBrandEntityModule,
+        PerfectScrollbarModule,
+        AppSidebarModule,
+        ModalModule.forRoot(),
+        TabsModule.forRoot(),
         NgxSpinnerModule,
         // jhipster-needle-angular-add-module JHipster will add new module here
     ],
@@ -100,4 +115,5 @@ import {
     ],
     bootstrap: [ JhiMainComponent ]
 })
-export class SenseBrandAppModule {}
+export class SenseBrandAppModule {
+}
