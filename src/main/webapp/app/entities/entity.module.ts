98fdb23d6d8d76af0041b500de35ae5b24707732
link <916969890@qq.com>
2018-05-01 21:05:10 +0800

    清空没用的代码, 做应用基层

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,23 +1,9 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
-import { SenseBrandDesignerModule } from './designer/designer.module';
-import { SenseBrandDesignerShowModule } from './designer-show/designer-show.module';
-import { SenseBrandDesignerShowImgModule } from './designer-show-img/designer-show-img.module';
-import { SenseBrandDesignerSentimentModule } from './designer-sentiment/designer-sentiment.module';
-import { SenseBrandDesignerAwardModule } from './designer-award/designer-award.module';
-import { SenseBrandDesignerIdeaDetailsModule } from './designer-idea-details/designer-idea-details.module';
-import { SenseBrandDesignerIdeaMediaModule } from './designer-idea-media/designer-idea-media.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     imports: [
-        SenseBrandDesignerModule,
-        SenseBrandDesignerShowModule,
-        SenseBrandDesignerShowImgModule,
-        SenseBrandDesignerSentimentModule,
-        SenseBrandDesignerAwardModule,
-        SenseBrandDesignerIdeaDetailsModule,
-        SenseBrandDesignerIdeaMediaModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
